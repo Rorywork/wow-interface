f0b377d2abd6e965cc171844069c25a1117df007
Rorywork <rorywork95@outlook.com>
2020-01-20 16:32:18 +0000

    Changed approach to the background

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,17 +4,30 @@ const tradeBox = document.getElementById("tradingInterface");
 
 const intro = document.getElementById("intro");
 
+const backPack = document.getElementById("backpack-space");
+
 
 
 function openTrade() {
     console.log("Open Trade btn working")
     intro.style.display = "none";
     tradeBox.style.visibility = "visible";
+}
 
+function closeTrade() {
+    console.log("Close trade btn working")
+    tradeBox.style.visibility = "hidden";
+    intro.style.display = "inline";
 }
 
 
 
 function openBag() {
     console.log("bag function working")
+
+    if (backPack.style.visibility === "visible") {
+        backPack.style.visibility = "hidden";
+    } else {
+        backPack.style.visibility = "visible";
+    }
 }
