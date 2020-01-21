649c7861e25fcb3cbb34c32f051d93d6ff5ee207
Rorywork <rorywork95@outlook.com>
2020-01-21 11:57:23 +0000

    Initial logic for adding item working for one instance

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,79 +6,73 @@ const backPack = document.getElementById("backpack-space");
 
 const snapper = document.getElementsByClassName("snapper");
 
-
 function item(name, level, quantity, value) {
-    this.name = name;
-    this.level = level;
-    this.quantity = quantity;
-    this.value = value;
+  this.name = name;
+  this.level = level;
+  this.quantity = quantity;
+  this.value = value;
 }
 
 laneSnapper = new item("Lane Snapper", 111, 200, 0.6);
 firelordsRobes = new item("Firelord's Robes", 108, 1, 17.48);
 demonStalkerGreathelm = new item("Demon Stalker Greathelm", 80, 1, 5.32);
 
-
-
 function openTrade() {
-    console.log("Open Trade btn working")
-    intro.style.display = "none";
-    tradeBox.style.visibility = "visible";
+  console.log("Open Trade btn working");
+  intro.style.display = "none";
+  tradeBox.style.visibility = "visible";
 }
 
 function closeTrade() {
-    console.log("Close trade btn working")
-    tradeBox.style.visibility = "hidden";
-    intro.style.display = "inline";
+  console.log("Close trade btn working");
+  tradeBox.style.visibility = "hidden";
+  intro.style.display = "inline";
 }
 
 function openBag() {
-    console.log("bag function working")
+  console.log("bag function working");
 
-    if (backPack.style.visibility === "visible") {
-        backPack.style.visibility = "hidden";
-    } else {
-        backPack.style.visibility = "visible";
-    }
+  if (backPack.style.visibility === "visible") {
+    backPack.style.visibility = "hidden";
+  } else {
+    backPack.style.visibility = "visible";
+  }
 }
 
-
 function addItem() {
-    onsole.log("Working");
-    document.getElementById("intro");
+  onsole.log("Working");
+  document.getElementById("intro");
 }
 snapper.onclick = addItem;
 const tradeItems = document.getElementsByClassName("item");
 
-
-
-
 function builder() {
-    console.log(tradeItems);
+  console.log(tradeItems);
 
-    console.log(tradeItems[0]);
-    //style = getComputedStyle(tradeItems[0]);
-    //console.log(style.backgroundColor);
-    // console.log(document.getElementsByClassName("item").style.backgroundColor);
+  console.log(tradeItems[0]);
+  //style = getComputedStyle(tradeItems[0]);
+  //console.log(style.backgroundColor);
+  // console.log(document.getElementsByClassName("item").style.backgroundColor);
 
-    for (i = 0; i < (tradeItems.length); i++) {
-        //console.log(tradeItems[i]);
+  for (i = 0; i < tradeItems.length; i++) {
+    //console.log(tradeItems[i]);
 
-        //tradeItems[i].style.backgroundColor = "red";
+    //tradeItems[i].style.backgroundColor = "red";
 
-        style = getComputedStyle(tradeItems[i]);
-        console.log(style.backgroundColor);
+    style = getComputedStyle(tradeItems[i]);
 
-        if (style.backgroundColor === "rgb(37, 37, 37)") {
-            console.log("success")
-        };
-        // console.log(tradeItems[i].style.backgroundColor);
-
-
-        //tradeItems[i].style.visibility = "hidden";
-
+    if (style.backgroundImage === "none") {
+      console.log("success");
+      //   tradeItems[i].style.backgroundColor = "white";
+      tradeItems[i].style.backgroundImage =
+        "url('/../dist/images/lane-snapper.PNG')";
+      tradeItems[i].style.backgroundPosition = "center";
+      tradeItems[i].style.backgroundRepeat = "no-repeat";
+      tradeItems[i].style.backgroundSize = "cover";
     }
+
+    break;
+  }
 }
 
-
-// if div.item.style.background-image === none 
+// if div.item.style.background-image === none
