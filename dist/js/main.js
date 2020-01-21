437f554393f4d02c1429845ed43c85401dd063aa
Rorywork <rorywork95@outlook.com>
2020-01-21 14:23:10 +0000

    clearTrade function working

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,19 +3,71 @@ const tradeBtn = document.getElementById("tradeBtn");
 const tradeBox = document.getElementById("tradingInterface");
 const intro = document.getElementById("intro");
 const backPack = document.getElementById("backpack-space");
-
 const snapper = document.getElementsByClassName("snapper");
 
-function item(name, level, quantity, value) {
-  this.name = name;
-  this.level = level;
-  this.quantity = quantity;
-  this.value = value;
-}
+const tradeItems = document.getElementsByClassName("item");
 
-laneSnapper = new item("Lane Snapper", 111, 200, 0.6);
-firelordsRobes = new item("Firelord's Robes", 108, 1, 17.48);
-demonStalkerGreathelm = new item("Demon Stalker Greathelm", 80, 1, 5.32);
+// Items that can be traded
+
+// function Item(name, id, level, quantity, value, image, clicked) {
+//   this.name = name;
+//   this.id = id;
+//   this.level = level;
+//   this.quantity = quantity;
+//   this.value = value;
+//   this.image = image;
+//   this.clicked = clicked;
+// }
+
+// let laneSnapper = new Item(
+//   "Lane Snapper",
+//   "lSnapper",
+//   111,
+//   200,
+//   0.6,
+//   "url('/../dist/images/lane-snapper.PNG')",
+//   false
+// );
+// let firelordsRobes = new Item(
+//   "Firelord's Robes",
+//   "fRobes",
+//   2,
+//   108,
+//   1,
+//   17.48,
+//   "url('/../dist/images/firelord-robes.PNG')",
+//   false
+// );
+// let demonStalkerGreathelm = new Item(
+//   "Demon Stalker Greathelm",
+//   "dSHelm",
+//   80,
+//   1,
+//   5.32,
+//   "url('/../dist/images/demon-stalker-helm.PNG')",
+//   false
+// );
+
+let myItems = [
+  [
+    "Lane Snapper",
+    "lSnapper",
+    "url('/../dist/images/lane-snapper.PNG')",
+    false
+  ],
+  [
+    "Firelord's Robes",
+    "fRobes",
+    "url('/../dist/images/firelord-robes.PNG')",
+    false
+  ],
+  [
+    "Demon Stalker Greathelm",
+    "dSHelm",
+    "url('/../dist/images/demon-stalker-helm.PNG')",
+    false
+  ]
+];
 
 function openTrade() {
   console.log("Open Trade btn working");
@@ -39,40 +91,67 @@ function openBag() {
   }
 }
 
-function addItem() {
-  onsole.log("Working");
-  document.getElementById("intro");
-}
-snapper.onclick = addItem;
-const tradeItems = document.getElementsByClassName("item");
+function clickItem(item_clicked) {
+  console.log(item_clicked);
 
-function builder() {
+  for (i = 0; i < myItems.length; i++) {
+    let item = myItems[i];
+
+    for (j = 0; j < item.length; j++) {
+      if (item[j] === item_clicked) {
+        console.log("We have a match!");
+        console.log(item);
+        let url = item[2];
+        console.log(url);
+        placeItem(url);
+      }
+    }
+  }
+}
+
+function placeItem(url) {
+  console.log("Working");
   console.log(tradeItems);
 
-  console.log(tradeItems[0]);
-  //style = getComputedStyle(tradeItems[0]);
-  //console.log(style.backgroundColor);
-  // console.log(document.getElementsByClassName("item").style.backgroundColor);
-
   for (i = 0; i < tradeItems.length; i++) {
-    //console.log(tradeItems[i]);
-
-    //tradeItems[i].style.backgroundColor = "red";
-
-    style = getComputedStyle(tradeItems[i]);
+    let style = getComputedStyle(tradeItems[i]);
 
     if (style.backgroundImage === "none") {
       console.log("success");
-      //   tradeItems[i].style.backgroundColor = "white";
-      tradeItems[i].style.backgroundImage =
-        "url('/../dist/images/lane-snapper.PNG')";
+      tradeItems[i].style.backgroundImage = url;
       tradeItems[i].style.backgroundPosition = "center";
       tradeItems[i].style.backgroundRepeat = "no-repeat";
       tradeItems[i].style.backgroundSize = "cover";
     }
-
-    break;
   }
 }
 
-// if div.item.style.background-image === none
+function clearTrade() {
+  for (i = 0; i < tradeItems.length; i++) {
+    let style = getComputedStyle(tradeItems[i]);
+
+    if (style.backgroundImage != "none") {
+      tradeItems[i].style.backgroundImage = "none";
+    }
+  }
+}
+
+// function builder() {
+//   console.log(tradeItems);
+//   console.log(tradeItems[0]);
+
+//   for (i = 0; i < tradeItems.length; i++) {
+//     style = getComputedStyle(tradeItems[i]);
+
+//     if (style.backgroundImage === "none") {
+//       console.log("success");
+//       tradeItems[i].style.backgroundImage =
+//         "url('/../dist/images/lane-snapper.PNG')";
+//       tradeItems[i].style.backgroundPosition = "center";
+//       tradeItems[i].style.backgroundRepeat = "no-repeat";
+//       tradeItems[i].style.backgroundSize = "cover";
+//     }
+
+//     break;
+//   }
+// }
